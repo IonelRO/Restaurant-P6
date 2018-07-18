8037f3589c020772ee23570fbf5fafbb92d0a00c
Ionel <dobre_ionel@yahoo.com>
2018-07-18 21:46:15 +0300

    Upload project files

---
+++ js/sw.js
@@ -0,0 +1,61 @@
+var staticCacheName = 'restaurant-reviews-v2';
+
+let urlToCache = [
+    '/',
+    './index.html',
+    './restaurant.html',
+    './css/styles.css',
+    './data/restaurants.json',
+    './img/1.jpg',
+    './img/2.jpg',
+    './img/3.jpg',
+    './img/4.jpg',
+    './img/5.jpg',
+    './img/6.jpg',
+    './img/7.jpg',
+    './img/8.jpg',
+    './img/9.jpg',
+    './img/10.jpg',
+    './js/main.js',
+    './js/restaurant_info.js',
+    './js/dbhelper.js',
+
+];
+self.addEventListener('install', (event) => {
+
+    event.waitUntil(
+        caches.open(staticCacheName).then(function (cache) {
+            console.log(cache);
+            return cache.addAll(urlToCache);
+
+        }).catch(erroe => {
+            console.log(erroe);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.filter(function (cacheName) {
+                    return cacheName.startsWith('restaurant-') &&
+                        cacheName != staticCacheName;
+                }).map(function (cacheName) {
+                    return caches.delete(cacheName);
+                })
+            );
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+});
